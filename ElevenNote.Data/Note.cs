8fce69013d4170af91c56a4d12ced3a6652d6435
Emilie Rains <emiliearains@gmail.com>
2021-04-01 21:47:10 -0400

    adds Starred feature 0.01 module

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -11,12 +12,19 @@ namespace ElevenNote.Data
     {
         [Key]
         public int NoteId { get; set; }
+
         [Required]
         public Guid OwnerId { get; set; }
+
         [Required]
         public string Title { get; set; }
+
         [Required] 
         public string Content { get; set; }
+
+        [DefaultValue(false)]
+        public bool IsStarred { get; set; }
+
         [Required]
         public DateTimeOffset CreatedUtc { get; set; }
 
